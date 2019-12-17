193e660e5d41860bbc43be5145895f9b7099e541
crusj <296769589@qq.com>
2019-12-17 10:57:56 +0800

    update 异步处理

--- service/pull.go
+++ service/pull.go
@@ -20,14 +20,16 @@ func Pull(ctx *fasthttp.RequestCtx) {
 			if _, err := os.Stat(path); os.IsNotExist(err) {
 				logger.Channel("unknown").Info("项目: %s,路径: %s,不存在", name, path)
 			} else {
-				cmd := exec.Command("/usr/bin/git", "pull", "origin", "master")
-				cmd.Dir = path
-				out, err := cmd.Output()
-				if err != nil {
-					logger.Channel("warn").Warn("项目: %s,路径: %s,错误,%s，%s ", name, path, err, out)
-				} else {
-					logger.Channel("default").Info("项目: %s,路径: %s，pull success,%s", name, path, out)
-				}
+				go func() {
+					cmd := exec.Command("/usr/bin/git", "pull", "origin", "master")
+					cmd.Dir = path
+					out, err := cmd.Output()
+					if err != nil {
+						logger.Channel("warn").Warn("项目: %s,路径: %s,错误,%s，%s ", name, path, err, out)
+					} else {
+						logger.Channel("default").Info("项目: %s,路径: %s，pull success,%s", name, path, out)
+					}
+				}()
 			}
 		}
 	}
